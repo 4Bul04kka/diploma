6494cb47e6832496abb5eb33ab3d59380ad6c672
4Bul04kka <november66.666.666@gmail.com>
2025-04-25 15:06:51 +0300

    начал писать бекенд

---
+++ shortcut/server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const PORT = 3001;
+
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.status(200).json("works");
+});
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
+});
